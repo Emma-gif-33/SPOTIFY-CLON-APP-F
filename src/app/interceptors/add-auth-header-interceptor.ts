d870c155546a4d51e5296033d561750f09670a89
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2025-10-14 12:53:21 -0600

    interceptor to retry added

--- src/app/interceptors/add-auth-header-interceptor.ts
+++ src/app/interceptors/add-auth-header-interceptor.ts
@@ -1,27 +1,23 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CookieStorageService } from '../services/cookie-storage-service';
 import { SpotifyLoginService } from '../services/spotify-api/spotify-login-service';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const addAuthHeaderInterceptor: HttpInterceptorFn = (req:HttpRequest<unknown>, next:HttpHandlerFn) => {
+  
+  if(!req.url.includes('api.spotify.com/v1'))
+    return next(req);
 
-  if(req.url.includes('api.spotify.com/v1')){  
-    const _cookieService: CookieStorageService = inject(CookieStorageService);
-    const _loginService: SpotifyLoginService = inject(SpotifyLoginService);
+  const _cookieService: CookieStorageService = inject(CookieStorageService);
+  const token = _cookieService.getCookieValue('access_token');
 
-    if(!_cookieService.isCookieValid('access_token')){
-      _loginService.getToken().subscribe();
+  const newReq = req.clone({
+    setHeaders:{
+      'Authorization': `Bearer ${token}`
     }
+  })
 
-    const access_token = _cookieService.getCookieValue('access_token');
-
-
-    const reqWHeaders = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${access_token}`),
-    })
-
-    return next(reqWHeaders)
-  }
-  return next(req);
+  return next(newReq);
     
 };
